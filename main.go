29fe161f0f3ffafd5041da498474ad8adb7df888
Marco Rabelink <rabem00@Marcos-MacBook-Pro.local>
2020-06-17 16:07:04 +0200

    all kind of test func

--- main.go
+++ main.go
@@ -6,8 +6,25 @@ import (
 	"code.gitea.io/sdk/gitea"
 )
 
+func createOrgRepo(client *gitea.Client, name string, description string) {
+	repos, err := client.SearchRepos(gitea.SearchRepoOptions{Keyword: name, Private: true})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%s\n", repos)
+	if len(repos) == 0 {
+		newOrgRepo, err := client.CreateOrgRepo("werkgebieden", gitea.CreateRepoOption{Name: name, Description: description, Private: true})
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("%s\n", newOrgRepo.Created)
+	}
+}
+
 func main() {
-	// Token: 3f0bf456ab473c30cdcc67b460989c30f015536c
+	// Token example: 3f0bf456ab473c30cdcc67b460989c30f015536c
 
 	// Setup new API connection
 	client := gitea.NewClient("http://192.168.2.40:3000", "3f0bf456ab473c30cdcc67b460989c30f015536c")
@@ -16,6 +33,7 @@ func main() {
 	org, err := client.GetOrg("werkgebieden")
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 	// Print field of organisation; fullname, description, etc
 	fmt.Printf("%s\n", org.FullName)
@@ -24,6 +42,7 @@ func main() {
 	repos, err := client.ListOrgRepos("werkgebieden", gitea.ListOrgReposOptions{})
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 	// Print name of each repo we got in repos
 	for _, repo := range repos {
@@ -32,4 +51,24 @@ func main() {
 		}
 	}
 
+	createOrgRepo(client, "w00005", "Werkgebieden-00005")
+
+	/*
+		newRepo, err := client.CreateRepo(gitea.CreateRepoOption{Name: "self", Description: "For my-self", Private: true})
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("%s\n", newRepo.Created)
+	*/
+
+	// New repo in orginisation (only Owner team)
+	/*
+		newOrgRepo, err := client.CreateOrgRepo("werkgebieden", gitea.CreateRepoOption{Name: "w00003", Description: "Werkgebieden-00003", Private: true})
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("%s\n", newOrgRepo.Created)
+	*/
 }
